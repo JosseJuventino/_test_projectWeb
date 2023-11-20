ad09bbfadbf808a1e9a161dfc414de6864983f44
JosseJuventino <00048322@uca.edu.sv>
2023-11-19 22:07:25 -0600

    adding ultimate changes

--- test_project/src/assets/views/ProjectsSearchView.jsx
+++ test_project/src/assets/views/ProjectsSearchView.jsx
@@ -1,7 +1,24 @@
+import Header from '../components/header/header';
+import Footer from '../components/footer/Footer';
+import InputSearch from '../components/input-search/InputSearch';
+import Dropdown from '../components/dropdown-buttons/Dropdown';
+
 function ProjectsSearchView() {
-    return(
-        <h1>Todo: project view</h1>
-    )
+    return (
+      <>
+        <Header />
+        <h1 className="text-center text-3xl mt-5">Proyectos</h1>
+        <div className="mx-44 my-10 flex flex-col">
+          <InputSearch />
+          <div className="mt-5 flex w-full flex-row justify-around">
+            <Dropdown />
+            <Dropdown />
+            <Dropdown />
+          </div>
+        </div>
+        <Footer />
+      </>
+    );
 }
 
 export default ProjectsSearchView;
